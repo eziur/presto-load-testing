ad9899fb818e362294fdd4600265bbdab6c56bbf
William Zhou <eziur.wz@gmail.com>
2025-03-20 11:44:33 -0700

    Add github actions

--- locustfile.py
+++ locustfile.py
@@ -1,4 +1,5 @@
-from locust import HttpUser, task, between
+import logging
+from locust import HttpUser, task, between, events
 from requests.auth import HTTPBasicAuth
 
 class User(HttpUser):
@@ -13,3 +14,17 @@ class User(HttpUser):
     @task
     def presto_clusters(self):
         self.client.get("/api/presto-clusters", auth=("presto", "T5uUk7cNEiaqDb0ady7E"))
+
+    @events.quitting.add_listener
+    def _(environment, **kw):
+        if environment.stats.total.fail_ratio > 0.05:
+            logging.error("Test failed due to failure ratio > 5%")
+            environment.process_exit_code = 1
+        elif environment.stats.total.avg_response_time > 1000:
+            logging.error("Test failed due to average response time ratio > 1000 ms")
+            environment.process_exit_code = 1
+        elif environment.stats.total.get_response_time_percentile(0.95) > 2000:
+            logging.error("Test failed due to 95th percentile response time > 2000 ms")
+            environment.process_exit_code = 1
+        else:
+            environment.process_exit_code = 0
