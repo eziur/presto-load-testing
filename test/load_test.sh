a452b9cb2ea8076020e985735e23bc83ae8c4987
William Zhou <eziur.wz@gmail.com>
2025-03-28 14:51:31 -0700

    Update for rz

--- test/load_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -xe
-
-(sleep 5 && open http://127.0.0.1:8089) &
-locust -f locustfile.py --users 100 --spawn-rate 10 --run-time 1m --stop-timeout 10s --autostart --web-port 8089 -H https://presto.ibm.prestodb.dev
