c9e64a6d0b84bb060c9007237afb42fc568f5d6e
William Zhou <eziur.wz@gmail.com>
2025-03-28 15:00:14 -0700

    Update for rz

--- test/run.sh
+++ test/run.sh
@@ -11,7 +11,7 @@ printenv | grep RZLT | sort
 open https://app.datadoghq.com/apm/traces\?query\=env%3Astaging%20operation_name%3Afastapi.request%20service%3Astaging-risk-webapp\&agg_m\=count\&agg_m_source\=base\&agg_t\=count\&cols\=core_service%2Ccore_resource_name%2Clog_duration%2Clog_http.method%2Clog_http.status_code\&fromUser\=false\&historicalData\=true\&messageDisplay\=inline\&query_translation_version\=v0\&refresh_mode\=sliding\&sort\=desc
 
 (sleep 2 && open http://127.0.0.1:8089) &
-locust --locustfile load_test/locustfile.py \
+locust --locustfile test/locustfile.py \
   --host ${RZLT_HOST} \
   --run-time ${RZLT_RUNTIME_MINUTES}m \
   --spawn-rate 10 \
