c3d50cabad7a9ebdcfda5d1bdbffd9093d4934ec
William Zhou <eziur.wz@gmail.com>
2025-04-03 15:37:50 -0700

    add unit tests

--- load_test/tests/unit/test_generate_coordinates.py
+++ load_test/tests/unit/test_generate_coordinates.py
@@ -1,8 +1,7 @@
-from load_test.src.locustfile import generate_rzrisk_payload
-from load_test.src import constants
+from load_test import constants, locustfile
 
 def test_generate_coordinates():
-    payload = generate_rzrisk_payload()
+    payload = locustfile.generate_rzrisk_payload()
     lat, lon = payload['location']['latitude'], payload['location']['longitude']
     assert constants.LAT_MIN <= lat <= constants.LAT_MAX, f"Latitude {lat} out of bounds"
     assert constants.LON_MIN <= lon <= constants.LON_MAX, f"Longitude {lon} out of bounds"
